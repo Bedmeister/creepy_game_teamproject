3cf4777114a3a5afaf262dc4d9b7f4cf75deb46d
Noah Timmons <ntimmonswork@gmail.com>
2025-03-15 16:33:12 -0500

    Fixed and

--- creepy_survival_game/Scripts/my_hero.gd
+++ creepy_survival_game/Scripts/my_hero.gd
@@ -3,19 +3,24 @@ extends CharacterBody2D
 
 # BASIC MOVEMENT
 var SPEED = 300.0
-var JUMP_VELOCITY = -400.0
-var movementSpeed = 3
 
+
+#furniture push physics
+var min_force = 15
+var max_force = 100
 
 
 func _physics_process(delta: float) -> void:
 	rotate_player()
-	_get_input();
+	slow_player()
+	_get_input()
 	move_and_slide()
+	furniture_physics()
+
 #Function is used to create diagonal movement
 func _get_input():
 	var  input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down");
-	velocity = input_direction * SPEED;
+	velocity = input_direction * SPEED
 
 
 
@@ -24,9 +29,16 @@ func rotate_player(): #rotates player with mouse
 	var direction = get_global_mouse_position()
 	look_at(direction)
 
+func slow_player():
+	if Input.is_action_pressed("pull"):
+		SPEED = 75
+	else: 
+		SPEED = 300
 
-func _on_furniture_detected(area: Area2D) -> void:
-	movementSpeed = 1
-
-func _on_furniture_left(area: Area2D) -> void:
-	movementSpeed = 3
+func furniture_physics():
+	for index in get_slide_collision_count():
+		var collision = get_slide_collision(index)
+		if collision.get_collider() is RigidBody2D:
+			var rigidbody = collision.get_collider() as RigidBody2D
+			var push_force = (15 * velocity.length() / SPEED) + min_force
+			rigidbody.apply_central_impulse(-collision.get_normal() * .25) 
